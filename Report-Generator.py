faf5a18aaa8e4d0658e804a59d083ac8c606cf9e
retynoli <retynoli@gmail.com>
2022-07-31 20:15:03 +0300

    N vector added

--- Report-Generator.py
+++ Report-Generator.py
@@ -3,21 +3,28 @@ import re
 # Import docx to work with .docx files.
 from docx import Document
 
-# Store file paths from input
-template_path = input('Write the full path to the template file: ')
-list_path = input('Write the full path to the list file: ')
-to_replace = input('Write a word to replace: ')
+# First, we create an empty lists to hold the path to all of lists files and words
+list_paths = []
+to_replace = []
 
-if template_path.endswith('.docx') and list_path.endswith('.docx'):
-    words = Document(list_path)
-    counter = 1
+# Store file paths from input
+number_of_lists = input('Write the lists amount: ')
+template_path = input('Write the full path to the template file: ')
+
+for i in range(0, int(number_of_lists)):
+   list_paths.append(input(f'\n[{i + 1}] Write the full path to the list file: '))
+   to_replace.append(input('Write a word to replace for this list: '))
+
+counter = 1
+if template_path.endswith('.docx') and list_paths[0].endswith('.docx'):
+    words = Document(list_paths[0])
     # Loop through replacer arguments
     for replacement in words.paragraphs:
         doc = Document(template_path)
         print(f"\n[{counter}] Сurrent replacement text: {replacement.text}")
         # initialize the number of occurrences of this word to 0
-        occurrences = {to_replace: 0}
-        
+        occurrences = {to_replace[0]: 0}
+
         # Loop through paragraphs
         for paragraph in doc.paragraphs:
             # Loop through runs (style spans)
@@ -25,13 +32,13 @@ if template_path.endswith('.docx') and list_path.endswith('.docx'):
                 # if there is text on this run, replace it
                 if run.text:
                     # get the replacement text
-                    replaced_text = re.sub(to_replace, replacement.text, run.text, 999)
+                    replaced_text = re.sub(to_replace[0], replacement.text, run.text, 999)
                     if replaced_text != run.text:
                         # if the replaced text is not the same as the original
                         # replace the text and increment the number of occurrences
                         run.text = replaced_text
-                        occurrences[to_replace] += 1
-                        
+                        occurrences[to_replace[0]] += 1
+
         # Loop through tables
         for table in doc.tables:
             for row in table.rows:
@@ -43,19 +50,20 @@ if template_path.endswith('.docx') and list_path.endswith('.docx'):
                             # if there is text on this run, replace it
                             if run.text:
                                 # get the replacement text
-                                replaced_text = re.sub(to_replace, replacement.text, run.text, 999)
+                                replaced_text = re.sub(to_replace[0], replacement.text, run.text, 999)
                                 if replaced_text != run.text:
                                     # if the replaced text is not the same as the original
                                     # replace the text and increment the number of occurrences
                                     run.text = replaced_text
-                                    occurrences[to_replace] += 1
-                    
+                                    occurrences[to_replace[0]] += 1
+
         # print the number of occurrences of each word
         for word, count in occurrences.items():
             print(f"The word {word} was found and replaced {count} time(s).")
 
         # make a new file name by changing the original file name with current word
         index = template_path.rfind('\\') + 1
+        # replace all system reserved symbols if found
         filename = replacement.text.translate({ord(c): "-" for c in '\/:*?"<>|'})
         new_file_path = template_path[:index] + filename + ".docx"
         print('File was saved at ' + new_file_path)
